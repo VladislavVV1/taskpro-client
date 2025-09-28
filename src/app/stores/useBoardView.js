35c25988e34e3e64f2db31abd96d640a24bd89c9
VladislavVV1 <vlad1905vvv@gmail.com>
2025-09-28 00:22:44 -0600

    remove double board const

--- src/app/stores/useBoardView.js
+++ src/app/stores/useBoardView.js
@@ -3,7 +3,6 @@ import { useColumnsStore } from '@/app/stores/columnStore';
 import { useCardsStore } from '@/app/stores/cardStore';
 
 export function useBoardView(boardId) {
-  const board = useBoardsStore(s =>(s.boards.find(b => b.id.toString() === boardId)));
   const board = useBoardsStore(s => (s.boards.find(b => b.id.toString() === boardId)));
   const cards = useCardsStore(s => s.cards);
   if (!board) return null;
